a5a4fa0ed9632c633521b6a7216f37db8cd84ef4
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-09-17 16:22:23 -0500

    Build out register endpoint

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,32 +1,16 @@
 const router = require('express').Router()
+const bcrypt = require('bcryptjs')
+const User = require('../users/users-model')
 
-router.post('/register', (req, res) => {
-  res.json('register')
-  /*
-    IMPLEMENT
-    You are welcome to build additional middleware to help with the endpoint's functionality.
-    DO NOT EXCEED 2^8 ROUNDS OF HASHING!
-
-    1- In order to register a new account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel", // must not exist already in the `users` table
-        "password": "foobar"          // needs to be hashed before it's saved
-      }
-
-    2- On SUCCESSFUL registration,
-      the response body should have `id`, `username` and `password`:
-      {
-        "id": 1,
-        "username": "Captain Marvel",
-        "password": "2a$08$jG.wIGR2S4hxuyWNcBf9MuoC4y0dNy7qC/LbmtuFBSdIhWks2LhpG"
-      }
-
-    3- On FAILED registration due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
-
-    4- On FAILED registration due to the `username` being taken,
-      the response body should include a string exactly as follows: "username taken".
-  */
+router.post('/register', async (req, res, next) => {
+  try {
+    const { username, password } = req.body
+    const hash = bcrypt.hashSync(password, 8)
+    const user = await User.add({ username, password: hash })
+    res.status(201).json(user)
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.post('/login', (req, res) => {
