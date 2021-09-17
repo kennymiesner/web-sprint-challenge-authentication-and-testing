6354a5273ec775b6b412b42cc979e126088f4b7a
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-09-17 16:28:10 -0500

    Build out login endpoint

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const bcrypt = require('bcryptjs')
 const User = require('../users/users-model')
+const buildToken = require('../middleware/build-token')
 
 router.post('/register', async (req, res, next) => {
   try {
@@ -13,31 +14,17 @@ router.post('/register', async (req, res, next) => {
   }
 })
 
-router.post('/login', (req, res) => {
-  res.json('login')
-  /*
-    IMPLEMENT
-    You are welcome to build additional middleware to help with the endpoint's functionality.
-
-    1- In order to log into an existing account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel",
-        "password": "foobar"
-      }
-
-    2- On SUCCESSFUL login,
-      the response body should have `message` and `token`:
-      {
-        "message": "welcome, Captain Marvel",
-        "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
-      }
-
-    3- On FAILED login due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
-
-    4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
-      the response body should include a string exactly as follows: "invalid credentials".
-  */
+router.post('/login', (req, res, next) => {
+  const { password } = req.body
+  if (bcrypt.compareSync(password, req.user.password)) {
+    const token = buildToken(req.user)
+    res.status(200).json({
+      message: `welcome, ${req.user.username}`,
+      token
+    })
+  } else {
+    next({ status: 401, message: 'invalid credentials' })
+  }
 })
 
 module.exports = router
